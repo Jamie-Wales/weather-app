8a0aa40c2c7242147b01bac19c388e410b0abd82
Jamie Wales <jamie.wales.developer@gmail.com>
2021-02-14 14:38:05 +0000

    stuff

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 const button = document.querySelector('.btn');
 const search = document.getElementById('weather-app__input');
+const results = document.querySelector('.weather-app__results');
 
-function generateWeatherApp(weather) {
- 
-    getMainWeather
-    
-    document.body.innerHTML = weather.main.temp
-}
 
 async function getWeatherData(searchterm) {
     
     const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchterm}&appid=fab001dc16d9e80e9c8227142cc821b7`);
     if (data.ok) {
         const weather = await data.json()
-        generateWeatherApp()
+        results.classList.remove('hidden');
+        generateWeatherApp(weather);
     } else {
         console.log('Oh no spagetti Os');
     }
 }
 
+function generateWeatherApp(weather) {
+    const title = document.querySelector('.weather-app__results__place');
+    const subtitle = document.querySelector('.weather-app__results__subtitle')
+    title.textContent = weather.name;
+    subtitle.textContent = weather.weather[0].description;
+    console.log(weather);
+
+    
+}
 
 button.addEventListener('click', (e) => {
     getWeatherData(search.value)
     search.value = '';
-
-    
 
 })
